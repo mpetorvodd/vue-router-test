70bb6366a126bb88516e39cd75c2df5a7d4ce73c
mpetrov <m.petrov@oddspedia.com>
2020-02-04 09:43:40 +0200

    Initial Commit

---
+++ src/components/Inner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="inner">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique sapiente repellat incidunt labore harum odit, iusto voluptate soluta? Veritatis, molestias.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Inner',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import HelloWorld from '@/components/HelloWorld'
+import Inner from '@/components/Inner'
+
+Vue.use(Router)
+
+const routes = [
+    {
+      path: '/', 
+      component: HelloWorld
+    },
+    {
+      path: 'inner',
+      component: Inner
+    }
+  ];
+
+const router = new Router({
+  routes,
+  mode: 'history'
+});
+
+router.beforeEach((to, from, next) => {
+  next();
+})
+
+export default router;
